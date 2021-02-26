b1177d82f16d8b9920d77559c5bb4028115e4ad5
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2021-02-26 17:48:31 -0300

    binare

---
+++ Nuevo/Converter.pas
@@ -0,0 +1,14 @@
+program Ejercicio;
+procedure PasarADecimal(var binario:integer);
+var
+    contador:integer;
+begin
+    
+end;
+var
+    Binario:integer;
+begin
+    writeln('Ingrese un numero Binario: ');
+    ReadLn(Binario);
+    PasarADecimal(Binario);
+end.
