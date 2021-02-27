675ab1fa0ad9cbecfe21b7abfcc330e0b2623f65
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2021-02-27 02:08:39 -0300

    puliendo

--- Nuevo/Converter.pas
+++ Nuevo/Converter.pas
@@ -30,7 +30,7 @@ end;
 //_____________________________________________________________________________________________
 procedure signo_operacion(Binario:cadena11;var numero:integer);
 var
-    i,dato:integer;
+    i:integer;
 begin
     for i:=0 to (Length(Binario)-2) do //Me quedo con los primeros digitos y dejando el ultimo porque se usa para el signo
     begin
@@ -38,6 +38,52 @@ begin
         begin
             numero:=numero+valores[i]; 
         end;
+    end;
+end;
+//_____________________________________________________________________________________________
+procedure ComplementoA2(ca1:cadena11;var ca2:cadena11);
+var
+    i:integer;
+    contador:cadena11;
+    carry:Boolean;
+    termino:Boolean;
+begin
+    termino:=False;
+    carry:=False;
+    contador:='';
+    for i:=0 to Length(ca1)-1 do 
+    begin
+        if termino then
+        begin
+            ca2:=ca1[Length(ca1)-i] +ca2;
+        end;
+        if carry then
+        begin
+            if ca1[Length(ca1)-i] = '0' then
+            begin
+                ca2:='1'+ca2;
+                carry:=False;
+                termino:=true;
+            end
+            else
+                begin
+                    ca2:='0'+ca2;
+                end;
+        end;
+        
+        if (i = 0) and (ca1[Length(ca1)-i] = '0') then //Miro el primer digito y tambien si es cero
+        begin
+            ca2:='1'+ca2;
+        end
+        else
+            begin
+                if (i = 0)then
+                begin
+                    ca2:='0'+ca2;
+                    carry:=true;
+                end;
+                
+            end;
     end;
 end;
 //_____________________________________________________________________________________________
@@ -49,7 +95,11 @@ var
     ca1:cadena11;
     bcs:integer;
     ca1_decimal:integer;
+    ca2:cadena11;
+    ca2_decimal:integer;
 begin
+    ca2:='';
+    ca2_decimal:=0;
     ca1_decimal:=0;
     bcs:=0;
     ca1:='';
@@ -77,8 +127,12 @@ begin
         WriteLn('Bcs_decimal: -',bcs);
         ComplementoA1(Binario,ca1);
         WriteLn('Ca1: ', ca1 );
-        WriteLn('Ca1_decimal: ', ca1_decimal );
-        WriteLn('Ca2: ', Binario );
+        signo_operacion(ca1,ca1_decimal);
+        WriteLn('Ca1_decimal: -', ca1_decimal );
+        ComplementoA2(ca1,ca2);
+        WriteLn('Ca2: ', ca2 );
+        signo_operacion(ca2,ca2_decimal);
+        WriteLn('Ca2_decimal: -', ca2_decimal );
     end;
     resultado:=dato;
 end;
