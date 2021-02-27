2852d311abf2b25e3d0a8f199b2798b1618ae751
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2021-02-27 01:12:53 -0300

    bcs

--- Nuevo/Converter.pas
+++ Nuevo/Converter.pas
@@ -6,9 +6,40 @@ type
 const
     valores : array[0..11] of integer = (1,2,4,8,16,32,64,128,256,512,1024,2048);
 //_____________________________________________________________________________________________
-procedure Exceso(Binario:cadena11;var ex:cadena11; CantBits:integer);
+{procedure Exceso(Binario:cadena11;var ex:cadena11; CantBits:integer);
 begin
   
+end;}
+//_____________________________________________________________________________________________
+procedure ComplementoA1(Binario:cadena11;var ca1:cadena11);
+var
+    i:integer;
+begin
+    for i:=0 to Length(Binario)-1 do //El menos 1 esta, porque se ejecuta una de mas owo
+    begin
+        if (Binario[Length(Binario)-i] = '1') then
+        begin
+            ca1:='0'+ca1;
+        end
+        else 
+            begin
+                ca1:='1'+ca1;
+            end;
+    end;
+end;
+//_____________________________________________________________________________________________
+procedure Bcs_decimal(Binario:cadena11;var bcs:integer);
+var
+    i,dato:integer;
+begin
+    dato:=0;
+    for i:=0 to (Length(Binario)-2) do //Me quedo con los primeros digitos y dejando el ultimo porque se usa para el signo
+    begin
+        if (Binario[Length(Binario)-i] = '1') then
+        begin
+            bcs:=bcs+valores[i]; 
+        end;
+    end;
 end;
 //_____________________________________________________________________________________________
 procedure Binario_A_Decimal(Binario:cadena11; var resultado:Integer);
@@ -16,15 +47,18 @@ var
     dato,i:integer;
     probando:Integer;
     ex2:cadena11;
+    ca1:cadena11;
+    bcs:integer;
 begin
+    bcs:=0;
+    ca1:='';
     dato:=0;
     probando:=0;
     for i:=0 to Length(Binario)-1 do //El menos 1 esta, porque se ejecuta una de mas owo
     begin
         if (Binario[Length(Binario)-i] = '1') then
         begin
-            dato:=dato+valores[i];
-            
+            dato:=dato+valores[i]; 
         end;
         probando:=i;
     end;
@@ -33,8 +67,16 @@ begin
         WriteLn('Bss: ', Binario );
         WriteLn('Ca1: ', Binario );
         WriteLn('Ca2: ', Binario );
-        Exceso(Binario,ex2);
-        
+        //Exceso(Binario,ex2);
+    end;
+    if ( Binario[Length(Binario)-probando] = '1')  then
+    begin
+        WriteLn('Bcs: ', Binario );
+        Bcs_decimal(Binario,bcs);
+        WriteLn('Bcs_Decimal: -',bcs);
+        ComplementoA1(Binario,ca1);
+        WriteLn('Ca1: ', ca1 );
+        WriteLn('Ca2: ', Binario );
     end;
     resultado:=dato;
 end;
@@ -48,5 +90,5 @@ begin
     writeln('Ingrese un numero Binario: ');
     ReadLn(Binario);
     Binario_A_Decimal(Binario,resultado);
-    WriteLn('El resultado es: ', resultado);
+    WriteLn('Decimal: ', resultado);
 end.
